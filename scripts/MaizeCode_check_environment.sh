a3d362b276054ce3bf8369c831d1a297d395324d
joncahn <73137478+joncahn@users.noreply.github.com>
2020-11-19 11:24:32 -0500

    Update MaizeCode_check_environment.sh

--- scripts/MaizeCode_check_environment.sh
+++ scripts/MaizeCode_check_environment.sh
@@ -17,9 +17,10 @@ usage="
 #####	-d: type of data [ChIP | RNA]
 ##### 	-h: help, returns usage
 #####
-##### The reference genome folder should contain a single fasta file and a single gff3 file (can be gzipped)
-##### The gff3 files should have 'gene' in column 3 and exons should be linked by 'Parent' in column 9
-##### The fasta and gff3 files should have the same chromosome names (i.e. 1 2 3... and 1 2 3... or Chr1 Chr2 Chr3... and Chr1 Chr2 Chr3...)
+##### The reference genome folder should contain a single fasta file (.fa or .fasta), a single GFF file (.gff [or .gff*]) and a single GTF file (can be gzipped)
+##### The GFF file should have 'gene' in column 3
+##### The GTF file can be produced using cufflinks with gffread -T <gff_file> -o <gtf_file>
+##### The fasta, GFF and GTF files should have the same chromosome names (i.e. 1 2 3... and 1 2 3... or Chr1 Chr2 Chr3... and Chr1 Chr2 Chr3...)
 ##### The type of data (ChIP or RNA) will define whether to build bowtie2 or STAR indexes
 #####
 ##### This script check if the indexes and other files need to be created and creates them if they do
@@ -60,79 +61,106 @@ if [ ! $ref ] || [ ! $pathtoref ]; then
 fi
 
 
-#### To check each reference genomes for fasta and gff3 files, and make indexes if needed
-### Not very great way (since it creates temporary unzipped fasta and gff3 files, even if not needed) but does the job quickly...
+#### To check each reference genomes for fasta, GFF3 and GTF files, and make indexes if needed
 printf "\nMaking sure all reference directories are ready or if genome indexes need to be built\n"
-ref_dir=$pathtoref/$ref
-if [ -s $ref_dir/*.fa.gz ]; then
-	fafile=$(ls $ref_dir/*.fa.gz)
-	fafilename=${fafile##*/}
-	printf "\nGzipped fasta file found in ${ref_dir}:\n $fafilename\n"
-	pigz -p $threads -dc $fafile > $ref_dir/temp_${ref}.fa
-	fasta=$ref_dir/temp_${ref}.fa
-elif [ -s $ref_dir/*.fa ]; then
-	fafile=$(ls $ref_dir/*.fa)
-	fafilename=${fafile##*/}
-	printf "\nUnzipped fasta file found in ${ref_dir}:\n $fafilename\n"
-	fasta=$fafile
+ref_dir=${pathtoref}/${ref}
+if [ -s ${ref_dir}/*.fa.gz ]; then
+	fa_file=$(ls ${ref_dir}/*.fa.gz)
+	fa_filename=${fa_file##*/}
+	printf "\nGzipped fasta file found in ${ref_dir}:\n ${fa_filename}\n"
+	pigz -p $threads -dc ${fa_file} > ${ref_dir}/temp_${ref}.fa
+	fasta=${ref_dir}/temp_${ref}.fa
+elif [ -s ${ref_dir}/*.fa ]; then
+	fa_file=$(ls $ref_dir/*.fa)
+	fa_filename=${fa_file##*/}
+	printf "\nUnzipped fasta file found in ${ref_dir}:\n ${fa_filename}\n"
+	fasta=${fa_file}
+elif [ -s ${ref_dir}/*.fasta.gz ]; then
+	fa_file=$(ls ${ref_dir}/*.fasta.gz)
+	fa_filename=${fa_file##*/}
+	printf "\nGzipped fasta file found in ${ref_dir}:\n ${fa_filename}\n"
+	pigz -p $threads -dc ${fa_file} > ${ref_dir}/temp_${ref}.fa
+	fasta=${ref_dir}/temp_${ref}.fa
+elif [ -s ${ref_dir}/*.fasta ]; then
+	fa_file=$(ls $ref_dir/*.fasta)
+	fa_filename=${fa_file##*/}
+	printf "\nUnzipped fasta file found in ${ref_dir}:\n ${fa_filename}\n"
+	fasta=${fa_file}
 else
 	printf "\nNo fasta file found in reference directory:\n ${ref_dir}\n"
 	exit 1
 fi
 
-if [ -s $ref_dir/*.gff3.gz ]; then
-	annofile=$(ls $ref_dir/*gff3.gz)
-	annofilename=${annofile##*/}
-	printf "\nGzipped annotation file found in ${ref_dir}:\n $annofilename\n"
-	pigz -p $threads -dc $annofile > $ref_dir/temp_${ref}.gff3	
-	gff=$ref_dir/temp_${ref}.gff3
-elif [ -s $ref_dir/*.gff3 ]; then
-	annofile=$(ls $ref_dir/*.gff3)
-	annofilename=${annofile##*/}
-	printf "\nUnzipped annotation file found in ${ref_dir}:\n $annofilename\n"
-	gff=$annofile
+if [ -s ${ref_dir}/*.gff*.gz ]; then
+	gff_file=$(ls ${ref_dir}/*gff*.gz)
+	gff_filename=${gff_file##*/}
+	printf "\nGzipped GFF annotation file found in ${ref_dir}:\n ${gff_filename}\n"
+	pigz -p $threads -dc ${gff_file} > ${ref_dir}/temp_${ref}.gff	
+	gff=${ref_dir}/temp_${ref}.gff
+elif [ -s ${ref_dir}/*.gff* ]; then
+	gff_file=$(ls ${ref_dir}/*.gff*)
+	gff_filename=${gff_file##*/}
+	printf "\nUnzipped GFF annotation file found in ${ref_dir}:\n ${gff_filename}\n"
+	gff=${gff_file}
 else
-	printf "\nNo annotation file (gff3) found in reference directory:\n ${ref_dir}\n"
+	printf "\nNo gff annotation file found in reference directory:\n ${ref_dir}\n"
 	exit 1
 fi
 
-#### To create - if needed - the genome index, a bed file of all genes (for deeptools compatibility) and chrom.sizes file
-if [ ! -s $ref_dir/chrom.sizes ]; then
+if [ -s ${ref_dir}/*.gtf.gz ]; then
+	gtf_file=$(ls ${ref_dir}/*gtf.gz)
+	gtf_filename=${gtf_file##*/}
+	printf "\nGzipped GTF annotation file found in ${ref_dir}:\n ${gtf_filename}\n"
+	pigz -p $threads -dc ${gtf_file} > ${ref_dir}/temp_${ref}.gtf	
+	gtf=${ref_dir}/temp_${ref}.gtf
+elif [ -s ${ref_dir}/*.gtf ]; then
+	gtf_file=$(ls ${ref_dir}/*.gtf)
+	gtf_filename=${gtf_file##*/}
+	printf "\nUnzipped GTF annotation file found in ${ref_dir}:\n ${gtf_filename}\n"
+	gtf=${gtf_file}
+else
+	printf "\nNo GTF annotation file found in reference directory:\n ${ref_dir}\n"
+	exit 1
+fi
+
+#### To create - if needed - the genome index, a bed file of all genes (for future analysis) and a chrom.sizes file
+if [ ! -s ${ref_dir}/chrom.sizes ]; then
 	printf "\nMaking chrom.sizes file for $ref\n"
 	samtools faidx $fasta
-	cut -f1,2 ${fasta}.fai > $ref_dir/chrom.sizes
+	cut -f1,2 ${fasta}.fai > ${ref_dir}/chrom.sizes
 fi
-	
+
 if [ ! -s $datatype/tracks/${ref}_all_genes.bed ]; then
 	printf "\nMaking a bed file with gene coordinates from $ref\n"
-	awk -v OFS="\t" '$3=="gene" {print $1,$4-1,$5,$9,".",$7}' $gff > $datatype/tracks/${ref}_all_genes.bed
+	awk -v OFS="\t" '$3=="gene" {print $1,$4-1,$5,$9,".",$7}' $gff > $datatype/tracks/${ref}_protein_coding_genes.bed
+	awk -v OFS="\t" '$3~"gene" {print $1,$4-1,$5,$9,".",$7}' $gff > $datatype/tracks/${ref}_all_genes.bed
 fi
 
 if [[ $datatype == "ChIP" ]]; then
 	if [ ! -s $datatype/reports/summary_mapping_stats.txt ]; then
-		printf "Line\tTissue\tMark\tRep\tTotal_reads\tPassing_filtering\tDeduplicated_reads\tProperly_mapped_reads\n" > $datatype/reports/summary_mapping_stats.txt
+		printf "Line\tTissue\tSample\tRep\tReference_genome\tTotal_reads\tPassing_filtering\tAll_mapped_reads\tUniquely_mapped_reads\n" > $datatype/reports/summary_mapping_stats.txt
 	fi
-	if [ ! -e $ref_dir/*.bt2* ]; then
+	if [ ! -e ${ref_dir}/*.bt2* ]; then
 #### This return the following warning '[: too many arguments' when the index is already build (several bt2 files)
 		printf "\nBuilding Bowtie2 index for $ref\n"
-		bowtie2-build --threads $threads $fasta $ref_dir/$ref
+		bowtie2-build --threads $threads $fasta ${ref_dir}/$ref
 	fi
 elif [[ $datatype == "RNA" ]]; then
 	if [ ! -s $datatype/reports/summary_mapping_stats.txt ]; then
-		printf "Line\tTissue\tMark\tRep\tReference_genome\tTotal_reads\tPassing_filtering\tOther\n" > $datatype/reports/summary_mapping_stats.txt
+		printf "Line\tTissue\tSample\tRep\tReference_genome\tTotal_reads\tPassing_filtering\tAll_mapped_reads\tUniquely_mapped_reads\n" > $datatype/reports/summary_mapping_stats.txt
 	fi
-	gen_dir=$ref_dir/STAR_index
+	gen_dir=${ref_dir}/STAR_index
 	if [ ! -d ${gen_dir} ]; then
-		printf "Building STAR index directory for $ref\n"
+		printf "\nBuilding STAR index directory for $ref\n"
 		mkdir ${gen_dir}
-		STAR --runThreadN $threads --runMode genomeGenerate --genomeDir ${gen_dir} --genomeFastaFiles $fasta --sjdbGTFfile $gff --sjdbGTFtagExonParentTranscript Parent
+		STAR --runThreadN $threads --runMode genomeGenerate --genomeDir ${gen_dir} --genomeFastaFiles $fasta --sjdbGTFfile $gtf
 	fi
 else
 	printf "\nType of data unknown!\n"
 	exit 1
 fi
 
-rm -f $ref_dir/temp*
+rm -f ${ref_dir}/temp*
 
 printf "\nScript finished successfully!\n"
 touch $datatype/chkpts/env_${ref}
