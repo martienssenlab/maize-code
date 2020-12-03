20d8120440b6c29745c3c88263925fa6539d93b7
joncahn <73137478+joncahn@users.noreply.github.com>
2020-12-03 09:52:13 -0500

    Update MaizeCode_R_Upset.r

--- scripts/MaizeCode_R_Upset.r
+++ scripts/MaizeCode_R_Upset.r
@@ -11,19 +11,9 @@ samplename<-args[2]
 set1<-colnames(inputable)
 set2<-set1[! set1 %in% c("PeakID","Distance")]
 
-plot1<-upset(inputable, 
-	     point.size = 2, 
-	     line.size = 1, 
-	     sets = set2, 
-	     order.by = "freq", 
-	     keep.order = TRUE,
-	     queries = list(
-			    list(query=elements, params=list("Distance","Gene_body"), color="red", active=T, query.name=c("Gene_body"))
-			    ), 
-	     query.legend = "top",
-	     mainbar.y.label = "Common peaks",
-	     sets.x.label = "Peaks"
-)
+plot1<-upset(inputable, point.size = 2, line.size = 1, sets = set2, order.by = "freq", keep.order = TRUE,
+      queries = list(list(query=elements, params=list("Distance","Gene_body"), color="red", active=T, 
+                          query.name=c("Gene_body"))), query.legend = "top")
 						  
 pdf(paste0("combined/plots/Upset_",samplename,".pdf"),10,8)
 print(plot1)
