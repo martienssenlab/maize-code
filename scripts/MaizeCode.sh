7bdb7345fd4f1f75e85a1e508e61a97332c80615
joncahn <73137478+joncahn@users.noreply.github.com>
2021-06-10 13:33:29 -0400

    Update MaizeCode.sh

--- scripts/MaizeCode.sh
+++ scripts/MaizeCode.sh
@@ -20,14 +20,15 @@ usage="
 ##### 	-h: help, returns usage
 #####
 ##### The samplefile should be a tab-delimited text file with 8 columns:
-##### col #1: Line (e.g. B73)
-##### col #2: Tissue (e.g endosperm) 
-##### col #3: Sample (e.g. H3K4me3 or 'Input' for ChIP, (sh)RNA or RAMPAGE for RNA). Histone marks must start with capital 'H'. 'Input', 'RNA' and 'RAMPAGE' are case-sensitive.
-##### col #4: Replicate ID [Rep1 | Rep2]
-##### col #5: SequencingID (e.g. S01). Unique identifier for the name of the sample in the raw sequencing folder which path is given in the next column. If downloading from SRA, put the SRR ID here.
-##### col #6: Path to the fastq files (e.g. /seq/Illumina_runs/NextSeqData/NextSeqOutput/190913_NB501555_0636_AH5HG7BGXC/Data/Intensities/BaseCalls/304846). If downloading from SRA, put 'SRA'.
-##### col #7: If data is paired-end or single-end [PE | SE]. 
-##### col #8: Name of the genome reference to map (e.g. B73_v4). Each genome reference should have a unique folder that contains a single fasta file and a single gff3 file (can be gzipped).
+##### col #1: Type of data [RNAseq | RAMPAGE | ChIPseq | TF_*] (shRNA in development). All options are case-sensitive. For TF_*, the star should be replaced by the name of the TF, e.g. TF_TB1.
+##### col #2: Line (e.g. B73)
+##### col #3: Tissue (e.g endosperm) 
+##### col #4: Sample (e.g. 'H3K4me3' or 'Input' for ChIP, 'IP' or 'Input' for TF CHIPseq, (sh)RNA or RAMPAGE for RNA).
+##### col #5: Replicate ID [Rep1 | Rep2]
+##### col #6: SequencingID (e.g. S01). Unique identifier for the name of the sample in the raw sequencing folder which path is given in the next column. If downloading from SRA, put the SRR ID here.
+##### col #7: Path to the fastq files (e.g. /seq/Illumina_runs/NextSeqData/NextSeqOutput/190913_NB501555_0636_AH5HG7BGXC/Data/Intensities/BaseCalls/304846). If downloading from SRA, put 'SRA'.
+##### col #8: If data is paired-end or single-end [PE | SE]. 
+##### col #9: Name of the genome reference to map (e.g. B73_v4). Each genome reference should have a unique folder that contains a single fasta file and a single gff3 file (can be gzipped).
 ##### The gff3 files should have 'gene' in column 3 and exons should be linked by 'Parent' in column 9
 ##### The fasta and gff3 files should have the same chromosome names (i.e. 1 2 3... and 1 2 3... or Chr1 Chr2 Chr3... and Chr1 Chr2 Chr3...)
 ##### For cleaner naming purposes, use '_samplefile.txt' as suffix
@@ -104,27 +105,30 @@ newref_list=()
 datat_ref_list=()
 new_env=0
 new_sample=0
-while read line tissue sample rep sampleID path paired ref
+while read data line tissue sample rep sampleID path paired ref
 do
 	name=${line}_${tissue}_${sample}_${rep}
-	case "$sample" in
-		H*|Input) datatype="ChIP";;
-		*RNA*|RAMPAGE) datatype="RNA";;
-		*) datatype="unknown";;
+	case "$data" in
+		ChIP) env="ChIP";;
+		RNAseq) env="RNA";;
+		RAMPAGE) env="RNA";;
+		shRNA) env="shRNA";;
+		TF_*) env="ChIP";;
+		*) env="unknown";;
 	esac
-	if [[ "$datatype" == "unknown" ]]; then
+	if [[ "$env" == "unknown" ]]; then
 		printf "Type of data unknown!\n"
 		printf "$usage\n"
 		exit 1
 	fi
-	if [ ! -e $datatype/chkpts/${name}_${ref} ]; then
+	if [ ! -e $env/chkpts/${name}_${ref} ]; then
 		new_sample=1
 	fi
-	if [ ! -e $datatype/chkpts/env_${ref} ]; then
+	if [ ! -e $env/chkpts/env_${ref} ]; then
 		new_env=1
-		newdatatype_list+=("$datatype")
+		newdatatype_list+=("$env")
 		newref_list+=("$ref")
-		data_ref_list+=("${datatype}_${ref}")
+		data_ref_list+=("${env}_${ref}")
 	fi
 done < $samplefile
 
@@ -139,23 +143,23 @@ else
 	pids=()
 	for ref in ${uniq_newref_list[@]}
 	do
-		for datatype in ${uniq_newdatatype_list[@]}
+		for env in ${uniq_newdatatype_list[@]}
 		do
-			if [[ " ${data_ref_list[@]} " =~ " ${datatype}_${ref} " ]]; then
-				check_list+=("$datatype/chkpts/env_${ref}")
-				if [ ! -d ./$datatype ]; then
-					mkdir ./$datatype
-					mkdir ./$datatype/fastq
-					mkdir ./$datatype/mapped
-					mkdir ./$datatype/tracks
-					mkdir ./$datatype/reports
-					mkdir ./$datatype/logs
-					mkdir ./$datatype/chkpts
+			if [[ " ${data_ref_list[@]} " =~ " ${env}_${ref} " ]]; then
+				check_list+=("$env/chkpts/env_${ref}")
+				if [ ! -d ./$env ]; then
+					mkdir ./$env
+					mkdir ./$env/fastq
+					mkdir ./$env/mapped
+					mkdir ./$env/tracks
+					mkdir ./$env/reports
+					mkdir ./$env/logs
+					mkdir ./$env/chkpts
 				fi
-				printf "\nPreparing environment of ${ref} genome for ${datatype} data\n"
-				qsub -sync y -N env_${ref}_${datatype} -o $datatype/logs/env_${ref}.log ${mc_dir}/MaizeCode_check_environment.sh -p $pathtoref -r $ref -d $datatype &
+				printf "\nPreparing environment of ${ref} genome for ${env} data\n"
+				qsub -sync y -N env_${ref}_${env} -o $env/logs/env_${ref}.log ${mc_dir}/MaizeCode_check_environment.sh -p $pathtoref -r $ref -d $env &
 				pids+=("$!")
-			elif [[ ! " ${data_ref_list[@]} " =~ " ${datatype}_${ref} " ]]; then
+			elif [[ ! " ${data_ref_list[@]} " =~ " ${env}_${ref} " ]]; then
 			### Combination datatype * ref does not exist in the sample file, moving on
 				:
 			elif [ ! -d $pathtoref/$ref ]; then
@@ -175,13 +179,13 @@ else
 	#### Check if the environment are good or if an error occurred
 	for check in ${check_list[@]}
 	do
-		datatype=${check%%/*}
+		env=${check%%/*}
 		ref=${check##*/env_}
 		if [ ! -e $check ]; then
-			printf "\nProblem while making environment for $datatype with $ref genome!\nCheck log: $datatype/logs/env_${ref}.log\n"
+			printf "\nProblem while making environment for $env with $ref genome!\nCheck log: $env/logs/env_${ref}.log\n"
 			exit 1
 		else
-			printf "\nEnvironment for $datatype with $ref genome now good to go\n"
+			printf "\nEnvironment for $env with $ref genome now good to go\n"
 			if [ -e ${pathtoref}/${ref}/temp_${ref}.fa ]; then
 				rm -f ${pathtoref}/${ref}/temp_${ref}.fa
 			fi
@@ -220,41 +224,58 @@ checkdatatype_list=()
 ref_list=()
 sample_list=()
 pids=()
-while read line tissue sample rep sampleID path paired ref
+while read line data tissue sample rep sampleID path paired ref
 do
 	ref_dir=$pathtoref/$ref
-	shortname=${line}_${tissue}_${sample}
-	name=${line}_${tissue}_${sample}_${rep}
-	case "$sample" in
-		H*|Input) datatype="ChIP";;
-		*RNA*|RAMPAGE) datatype="RNA";;
+	case "$data" in
+		ChIP) 	env="ChIP"
+			script="ChIP"
+			shortname=${line}_${tissue}_${sample}
+			name=${line}_${tissue}_${sample}_${rep};;
+		RNAseq) env="RNA"
+			script="RNA"
+			shortname=${line}_${tissue}_${sample}
+			name=${line}_${tissue}_${sample}_${rep};;
+		RAMPAGE) env="RNA"
+			script="RNA"
+			shortname=${line}_${tissue}_${sample}
+			name=${line}_${tissue}_${sample}_${rep};;
+		shRNA) env="shRNA"
+			script="shRNA"
+			shortname=${line}_${tissue}_${sample}
+			name=${line}_${tissue}_${sample}_${rep};;
+		TF_*) env="ChIP"
+			script="TF"
+			tmp=${data##TF_}
+			shortname=${line}_${tmp}_${sample}
+			name=${line}_${tmp}_${sample}_${rep};;
 	esac
-	check=$datatype/chkpts/${name}_${ref}
+	check=$env/chkpts/${name}_${ref}
 	ref_list+=("$ref")
 	if [ -e $check ]; then
 		printf "Sample $name has already been mapped to $ref genome\n"
 	else
 		checkname_list+=("$name")
-		checkdatatype_list+=("$datatype")
+		checkdatatype_list+=("$env")
 		check_list+=("$check")
-		if ls ./$datatype/fastq/trimmed_${name}*.fastq.gz 1> /dev/null 2>&1; then
+		if ls ./$env/fastq/trimmed_${name}*.fastq.gz 1> /dev/null 2>&1; then
 			printf "\nTrimmed fastq file(s) for ${name} already exist\n"
 			export step="done"
-		elif ls ./$datatype/fastq/${name}*.fastq.gz 1> /dev/null 2>&1; then
+		elif ls ./$env/fastq/${name}*.fastq.gz 1> /dev/null 2>&1; then
 			printf "\nFastq file(s) for ${name} already exist\n"
 			export step="trim"
 		else
 			printf "\nNew sample ${name} to be copied/downloaded\n"
 			export step="download"
 		fi
-		printf "\nRunning $datatype mapping script for $name on $ref genome\n"
-		cd $datatype
-		qsub -sync y -N ${name} -o logs/${name}.log ${mc_dir}/MaizeCode_${datatype}_sample.sh -d $ref_dir -l $line -t $tissue -m $sample -r $rep -i $sampleID -f $path -p $paired -s $step &
+		printf "\nRunning $script mapping script for $name on $ref genome\n"
+		cd $env
+		qsub -sync y -N ${name} -o logs/${name}.log ${mc_dir}/MaizeCode_${script}_sample.sh -d $ref_dir -x $data -l $line -t $tissue -m $sample -r $rep -i $sampleID -f $path -p $paired -s $step &
 		pids+=("$!")
 		cd ..
 	fi
 	if [[ ! "${sample_list[@]}" =~ "${shortname}" ]] && [[ "${sample}" != "Input" ]]; then
-		printf "${line}\t${tissue}\t${sample}\t${paired}\t${ref_dir}\n" >> $analysisfile
+		printf "${data}\t${line}\t${tissue}\t${sample}\t${paired}\t${ref_dir}\n" >> $analysisfile
 		sample_list+=("$shortname")
 	fi
 done < $samplefile
@@ -284,13 +305,21 @@ if [ -e combined/reports/temp_mapping_stats_${samplename}.txt ]; then
 	rm -f combined/reports/temp_mapping_stats_${samplename}.txt
 fi
 
-while read line tissue sample rep sampleID path paired ref
+while read data line tissue sample rep sampleID path paired ref
 do
-	case "$sample" in
-		H*|Input) datatype="ChIP";;
-		*RNA*|RAMPAGE) datatype="RNA";;
+	case "$data" in
+		ChIP) env="ChIP"
+			name="${tissue}";;
+		RNAseq) env="RNA"
+			name="${tissue}";;
+		RAMPAGE) env="RNA"
+			name="${tissue}";;
+		shRNA) env="shRNA"
+			name="${tissue}";;
+		TF_*) env="ChIP"
+			name=${data##TF_};;
 	esac
-	awk -v a=$line -v b=$tissue -v c=$sample -v d=$rep -v e=$ref '$1==a && $2==b && $3==c && $4==d && $5==e' ${datatype}/reports/summary_mapping_stats.txt >> combined/reports/temp_mapping_stats_${samplename}.txt
+	awk -v a=$line -v b=$name -v c=$sample -v d=$rep -v e=$ref '$1==a && $2==b && $3==c && $4==d && $5==e' ${env}/reports/summary_mapping_stats.txt >> combined/reports/temp_mapping_stats_${samplename}.txt
 done < $samplefile
 
 printf "Line\tTissue\tSample\tRep\tReference_genome\tTotal_reads\tPassing_filtering\tAll_mapped_reads\tUniquely_mapped_reads\n" > combined/reports/summary_mapping_stats_${samplename}.txt
