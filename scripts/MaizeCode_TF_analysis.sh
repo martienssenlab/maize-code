8f05573881628c3350958011a69b9576eee731b4
joncahn <73137478+joncahn@users.noreply.github.com>
2021-07-12 10:54:30 -0400

    Update MaizeCode_TF_analysis.sh

--- scripts/MaizeCode_TF_analysis.sh
+++ scripts/MaizeCode_TF_analysis.sh
@@ -252,9 +252,8 @@ do
 		#### v1="selected" peaks (best peaks from selected, i.e. in merged and both pseudo reps)		
 		printf "\nGetting peak fasta sequences for $name meme v1\n"
 		awk -v OFS="\t" '($1~/^[0-9]/ || $1~/^chr[0-9]/ || $1~/^Chr[0-9]/ ) {a=$2+$10; print $1,a-50,a+50,$4}' peaks/best_peaks_${name}.bed > peaks/selected_motifs_${name}.bed
-		bedtools intersect -v -a peaks/selected_motifs_${name}.bed -b tracks/${ref}_masked_regions.bed > peaks/temp_selected_motifs_${name}.bed
-		bedtools getfasta -name -fi ${ref_dir}/${ref}.fa -bed peaks/temp_selected_motifs_${name}.bed > peaks/selected_sequences_${name}.fa
-		rm -f peaks/temp_selected_motifs_${name}.bed
+		bedtools intersect -v -a peaks/selected_motifs_${name}.bed -b tracks/${ref}_masked_regions.bed > peaks/masked_selected_motifs_${name}.bed
+		bedtools getfasta -name -fi ${ref_dir}/${ref}.fa -bed peaks/masked_selected_motifs_${name}.bed > peaks/selected_sequences_${name}.fa
 		printf "\nGetting motifs for $name with meme\n"
 		meme-chip -oc motifs/${name}/meme3 -meme-p $threads -meme-nmotifs 10 -streme-nmotifs 10 peaks/selected_sequences_${name}.fa
 		printf "\nLooking for similar motifs in JASPAR database with tomtom\n"
@@ -275,9 +274,8 @@ do
 		#### v2="selected" peaks (peaks in both biological reps, i.e all peaks in idr)
 		printf "\nGetting peak fasta sequences for $name meme v2\n"
 		awk -v OFS="\t" '($1~/^[0-9]/ || $1~/^chr[0-9]/ || $1~/^Chr[0-9]/ ) {a=$2+$10; print $1,a-50,a+50}' peaks/idr_${name}.narrowPeak > peaks/selected_motifs_${name}.bed
-		bedtools intersect -v -a peaks/selected_motifs_${name}.bed -b tracks/${ref}_masked_regions.bed > peaks/temp_selected_motifs_${name}.bed
-		bedtools getfasta -name -fi ${ref_dir}/${ref}.fa -bed peaks/selected_motifs_${name}.bed > peaks/selected_sequences_${name}.fa
-		rm -f peaks/temp_selected_motifs_${name}.bed
+		bedtools intersect -v -a peaks/selected_motifs_${name}.bed -b tracks/${ref}_masked_regions.bed > peaks/masked_selected_motifs_${name}.bed
+		bedtools getfasta -name -fi ${ref_dir}/${ref}.fa -bed peaks/masked_selected_motifs_${name}.bed > peaks/selected_sequences_${name}.fa
 		printf "\nGetting motifs for $name with meme\n"
 		meme-chip -oc motifs/${name}/meme4 -meme-p $threads -meme-nmotifs 10 -streme-nmotifs 10 peaks/selected_sequences_${name}.fa
 		printf "\nLooking for similar motifs in JASPAR database with tomtom\n"
