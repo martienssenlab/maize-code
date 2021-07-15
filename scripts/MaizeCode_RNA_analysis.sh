8e3d159c83a1ccba2db33801440b5c451cbe0d31
joncahn <73137478+joncahn@users.noreply.github.com>
2021-07-15 16:21:15 -0400

    Update MaizeCode_RNA_analysis.sh

--- scripts/MaizeCode_RNA_analysis.sh
+++ scripts/MaizeCode_RNA_analysis.sh
@@ -73,10 +73,8 @@ do
 	case "$rnatype" in
 		RNAseq)	export param_bg="--outWigType bedGraph"
 				export strandedness="reverse";;
-		shRNA) 	export param_bg="--outWigType bedGraph"
-				export strandedness="reverse";;
 		RAMPAGE)	export param_bg="--outWigType bedGraph read1_5p"
-					export strandedness="forward";;
+				export strandedness="reverse";;
 	esac
 	printf "\nStarting single RNA sample analysis for $name\n"	
 	export ref_dir=${ref_dir}
