7fdedb02d90c1a7ee5d2d32801c95a692001f982
joncahn <73137478+joncahn@users.noreply.github.com>
2021-09-01 13:47:09 -0400

    Update MaizeCode_R_GO.r

--- scripts/MaizeCode_R_GO.r
+++ scripts/MaizeCode_R_GO.r
@@ -6,39 +6,9 @@ library(rrvgo)
 library(dplyr)
 library(topGO)
 library(purrr)
+library(org.Zmays.eg.db)
 
 args = commandArgs(trailingOnly=TRUE)
-
-#### If database has to be built.
-# info<-read.delim("combined/GO/B73_v4_infoGO.tab", header=FALSE)
-# genes<-read.delim("B73_genes_info.tab", header=TRUE) %>%
-#  rowwise() %>%
-#  mutate(desc=ifelse(Description=="protein_coding",Type,Description),
-#         typ=ifelse(Description=="protein_coding",Description,Type)) %>%
-#  select(-Description, -Type) %>%
-#  rename(Description=desc, Type=typ)
-
-# fGOzm<-info[,c(2,5,7)]
-# colnames(fGOzm)<-c("GID","GO","EVIDENCE")
-
-# fSymzm<-select(genes, GID, Type, Description)
-# fSymzm$ENTREZID <- paste0("ent",fSymzm$GID)
-
-# fChrzm<-select(genes, GID, Chr)
-
-# makeOrgPackage(gene_info=fSymzm, chromosome=fChrzm, go=fGOzm,
-#               version="0.1",
-#               maintainer="user <user@maizecode>",
-#               author="user <user@maizecode>",
-#               outputDir = ".",
-#               tax_id = "381124",
-#               genus = "Zea",
-#               species = "mays",
-#               goTable="go")
-
-# install.packages("./combined/GO/org.Zmays.eg.db", repos=NULL, type="source")
-
-library(org.Zmays.eg.db)
 
 genecount<-read.delim(args[1], header = TRUE, row.names = "gene_ID")
 sampletable<-read.delim(args[2], header = FALSE)
@@ -92,18 +62,17 @@ plotGOs<-function(TopGoResults, ont, name) {
                                 orgdb="org.Zmays.eg.db",
                                 ont=ont,
                                 method="Rel")
-  if ( !is.null(dim(simMatrix)) ) {
-    scores<-setNames(-log10(as.numeric(TopGoResults$classicFisher)), TopGoResults$GO.ID)
-    reducedTerms<-reduceSimMatrix(simMatrix,
-                                  scores,
-                                  threshold = 0.7,
-                                  orgdb="org.Zmays.eg.db")
-    # pdf(paste0("combined/plots/topGO_",ont,"_",name,"_scatter.pdf"), width=8, height=8)
-    # scatterPlot(simMatrix, reducedTerms, size = "score")
-    # dev.off()
-    pdf(paste0("combined/plots/topGO_",name,"_",ont,"_treemap.pdf"), width=8, height=8)
-    treemapPlot(reducedTerms, size = "score")
-    dev.off()
+  scores<-setNames(-log10(as.numeric(TopGoResults$classicFisher)), TopGoResults$GO.ID)
+  reducedTerms<-reduceSimMatrix(simMatrix,
+                                scores,
+                                threshold = 0.7,
+                                orgdb="org.Zmays.eg.db")
+  # pdf(paste0("combined/plots/topGO_",ont,"_",name,"_scatter.pdf"), width=8, height=8)
+  # scatterPlot(simMatrix, reducedTerms, size = "score")
+  # dev.off()
+  pdf(paste0("combined/plots/topGO_",name,"_",ont,"_treemap.pdf"), width=8, height=8)
+  treemapPlot(reducedTerms, size = "score")
+  dev.off()
   }
 }
 
