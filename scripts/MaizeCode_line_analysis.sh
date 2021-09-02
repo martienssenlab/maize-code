303192cd94c70d2e28475f1d10c1856f396eed8e
joncahn <73137478+joncahn@users.noreply.github.com>
2021-09-02 17:59:36 -0400

    Update MaizeCode_line_analysis.sh

--- scripts/MaizeCode_line_analysis.sh
+++ scripts/MaizeCode_line_analysis.sh
@@ -573,8 +573,9 @@ if [[ "${total}" == "No" ]]; then
 	exit 0
 fi
 
-if [ ${#chip_sample_list[@]} -lt 1 ]; then
-	printf "\nNot enough ChIPseq samples for deeptools analysis for ${analysisname}\nAnalysis is thus finished!\n"
+total_sample_number=$((${#chip_sample_list[@]} + ${#rnaseq_sample_list[@]} + ${#rampage_sample_list[@]} + ${#shrna_sample_list[@]}))
+if [ ${total_sample_number} -lt 1 ]; then
+	printf "\nNot enough samples for deeptools analysis for ${analysisname}\nAnalysis is thus finished!\n"
 	touch combined/chkpts/analysis_${analysisname}
 	exit 0
 fi
