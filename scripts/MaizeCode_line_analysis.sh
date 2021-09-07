d81ae31818aa6b1703aab522e2bfb2c1e92399f9
joncahn <73137478+joncahn@users.noreply.github.com>
2021-09-07 15:08:57 -0400

    Update MaizeCode_line_analysis.sh

--- scripts/MaizeCode_line_analysis.sh
+++ scripts/MaizeCode_line_analysis.sh
@@ -768,7 +768,9 @@ if [ ${#rnaseq_name_list[@]} -ge 2 ]; then
 				awk -v OFS="\t" -v d=${DEG} '$11==d' ${file} > combined/matrix/temp_regions_${analysisname}_DEG_${filename}_${DEG}.bed
 			done
 			regions_files+=("combined/matrix/temp_regions_${analysisname}_DEG_${filename}_UP.bed" "combined/matrix/temp_regions_${analysisname}_DEG_${filename}_DOWN.bed")
-			regions_labels+=("${filename}_UP" "${filename}_DOWN")
+			nbup=$(wc -l combined/matrix/temp_regions_${analysisname}_DEG_${filename}_UP.bed)
+			ndbown=$(wc -l combined/matrix/temp_regions_${analysisname}_DEG_${filename}_DOWN.bed)
+			regions_labels+=("${filename}_UP(${nbup})" "${filename}_DOWN(${nbdown})")
 			awk -v OFS="\t" 'NR>1 {print $1,$2,$3}' ${file} >> combined/matrix/temp_regions_${analysisname}_all_DEGs.bed		
 		done
 		sort -k1,1n -k2,2n combined/matrix/temp_regions_${analysisname}_all_DEGs.bed -u > combined/matrix/temp_regions_${analysisname}_all_DEGs_unique.bed
@@ -798,6 +800,8 @@ if [ ${#rnaseq_name_list[@]} -ge 2 ]; then
 			for namei in ${rnaseq_name_list[@]}
 			do
 				filenames="combined/DEG/only_${namei}_DEG_UP_${analysisname}.bed combined/DEG/only_${namei}_DEG_DOWN_${analysisname}.bed"
+				nbup=$(wc -l combined/DEG/only_${namei}_DEG_UP_${analysisname}.bed)
+				nbdown=$(wc -l combined/DEG/only_${namei}_DEG_DOWN_${analysisname}.bed)
 				printf "\nComputing matrix for ${namei} specific DEG from ${analysisname}\n"
 				computeMatrix scale-regions --missingDataAsZero --skipZeros -R ${filenames} -S ${sorted_marks[@]} -bs 50 -b 2000 -a 2000 -m 5000 -p $threads -o combined/matrix/${analysisname}_only_${namei}_DEG.gz --quiet
 				printf "\nGetting scales for the ${tissue} specific DEG matrix of ${analysisname}\n"
@@ -811,7 +815,7 @@ if [ ${#rnaseq_name_list[@]} -ge 2 ]; then
 					maxi=$(grep ${sample} combined/matrix/values_${analysisname}_only_${namei}_DEG.gz | awk '{print $6}')
 					maxs+=("${maxi}")
 				done
-				plotProfile -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/temp_regions_${analysisname}_profile_only_${namei}_DEG.pdf --plotType 'lines' --averageType 'mean' --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP" "${namei}_DOWN" --outFileNameData combined/matrix/values_${analysisname}_profile_only_${namei}_DEG.txt
+				plotProfile -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/regions_${analysisname}_profile_only_${namei}_DEG.pdf --plotType 'lines' --averageType 'mean' --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP" "${namei}_DOWN" --outFileNameData combined/matrix/values_${analysisname}_profile_only_${namei}_DEG.txt
 				ymins=()
 				ymaxs=()
 				for sample in ${sorted_labels[@]}
@@ -822,8 +826,8 @@ if [ ${#rnaseq_name_list[@]} -ge 2 ]; then
 					ymaxs+=("${ymaxi}")
 				done
 				printf "\nPlotting heatmap for ${namei} specific DEG from ${analysisname}\n"
-				plotHeatmap -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/${analysisname}_heatmap_only_${namei}_DEG.pdf --sortRegions descend --sortUsing mean --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP" "${namei}_DOWN" --zMin ${mins[@]} --zMax ${maxs[@]} --yMin ${ymins[@]} --yMax ${ymaxs[@]} --colorMap 'seismic' --interpolationMethod 'bilinear'
-				plotProfile -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/regions_${analysisname}_profile_only_${namei}_DEG.pdf --plotType 'lines' --averageType 'mean' --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP" "${namei}_DOWN" --yMin ${ymins[@]} --yMax ${ymaxs[@]}
+				plotHeatmap -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/${analysisname}_heatmap_only_${namei}_DEG.pdf --sortRegions descend --sortUsing mean --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP(${nbup})" "${namei}_DOWN(${nbdown})" --zMin ${mins[@]} --zMax ${maxs[@]} --yMin ${ymins[@]} --yMax ${ymaxs[@]} --colorMap 'seismic' --interpolationMethod 'bilinear'
+				plotProfile -m combined/matrix/${analysisname}_only_${namei}_DEG.gz -out combined/plots/regions_${analysisname}_profile_only_${namei}_DEG.pdf --plotType 'lines' --averageType 'mean' --samplesLabel ${sorted_labels[@]} --regionsLabel "${namei}_UP(${nbup})" "${namei}_DOWN(${nbdown})" --yMin ${ymins[@]} --yMax ${ymaxs[@]}
 			done
 		fi
 	fi
