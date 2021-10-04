d9d0d2e31b3be840f8c9e2acd8cf4b5aca2ba762
joncahn <73137478+joncahn@users.noreply.github.com>
2021-10-04 14:17:36 -0400

    Update and rename MaizeCode_R_TSS_distribution.r to MaizeCode_R_TSS_distribution_upset.r

---
+++ scripts/MaizeCode_R_TSS_distribution_upset.r
@@ -0,0 +1,114 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(RColorBrewer)
+library(cowplot)
+library(ComplexUpset)
+library(purrr)
+
+args = commandArgs(trailingOnly=TRUE)
+
+analysisname<-args[1]
+
+### For distribution plot
+table<-read.delim(args[2], header = TRUE)
+table$Gene<-as.factor(table$Gene)
+table$TE<-as.factor(table$TE)
+table$Label<-factor(table$Label, 
+                     levels = c("helitron","LINE_element","LTR_retrotransposon",
+                                "SINE_element","solo_LTR","terminal_inverted_repeat_element","Intergenic","Terminator","Gene_body",
+                                "Promoter"))
+table$Labelcombined<-as.factor(table$Labelcombined)
+
+plot1<-ggplot(table, aes(Tissue, fill=Label)) +
+  geom_bar(stat="count", position="stack", colour="black", show.legend = F) +
+  labs(title="", x="",y="Number of peaks") +
+  scale_fill_manual(values = brewer.pal(10,"Paired")) +
+  theme(panel.grid=element_blank(),
+        panel.grid.major.y = element_line(colour="grey"),
+        axis.ticks=element_blank(),
+        panel.background=element_blank(),
+		axis.text.x=element_blank())
+plot1
+
+plot2<-ggplot(table, aes(Tissue, fill=Label)) +
+  geom_bar(stat="count", position="fill", colour="black", show.legend = T) +
+  labs(title="", x="",y="Percentage", fill="Genomic feature") +
+  scale_fill_manual(values = brewer.pal(10,"Paired")) +
+  theme(panel.grid=element_blank(),
+        panel.grid.major.y = element_line(colour="grey"),
+        axis.ticks=element_blank(),
+        panel.background=element_blank(),
+        axis.text.x=element_text(size=15))
+plot2
+
+title<-ggdraw() +
+  draw_label(paste0("Distribution of RAMPAGE peaks in ",analysisname), 
+             size = 20, fontface="bold")
+
+pdf(paste0("combined/plots/TSS_distribution_",analysisname,".pdf"), height=10, width=12)
+plot_grid(title, plot1, plot2, ncol=1, align="v", axis="lr", rel_heights = c(0.2,1,2))
+dev.off()
+
+### For Upset plot
+
+inputable<-read.delim(args[3], header = TRUE)
+inputable$Label<-factor(inputable$Label, levels = c("helitron","LINE_element","LTR_retrotransposon",
+                                "SINE_element","solo_LTR","terminal_inverted_repeat_element","Intergenic","Terminator","Gene_body",
+                                "Promoter"))
+set1<-colnames(inputable)
+sampleCols<-set1[! set1 %in% c("Line","Peak_ID","Gene","TE","Label","Labelcombined")]
+
+plot<-upset(inputable, sampleCols, name="TSS", 
+      mode='exclusive_intersection',
+      n_intersections=10, 
+      sort_sets=FALSE,
+      height_ratio = 0.75,
+      base_annotations = list(
+        'Shared TSS'=intersection_size(
+          counts=FALSE, mapping=aes(fill=Label)) +
+          scale_fill_manual(values=c("Intergenic"="#B8B5B3","Terminator"="#B233FF",
+                                     "Gene_body"="#3358FF","Promoter"="#FF33E0","helitron"="#0B6D10","LINE_element"="#39953D","LTR_retrotransposon"="#08AF0F",
+                                "SINE_element"="#39E540","solo_LTR"="#92EB96","terminal_inverted_repeat_element"="#05F910"),
+                            name="Genomic feature")
+      ),
+      set_sizes = (upset_set_size() + ylab("Total TSS") +
+        theme(axis.text.x = element_text(angle = 45))),
+      matrix = (intersection_matrix(geom = geom_point(shape = "circle",size = 3),
+          segment = geom_segment(size = 1.5),
+          outline_color = list(active = alpha("white", 0),inactive = alpha("white", 0))) +
+          scale_color_manual(values = c("TRUE" = "black", "FALSE" = alpha("white", 0)),
+            labels = c("TRUE" = "yes", "FALSE" = "no"),
+            breaks = c("TRUE", "FALSE"),
+            guide = FALSE) +
+          theme(axis.ticks = element_blank(),
+            panel.grid = element_blank())
+      ),
+      themes = upset_modify_themes(
+        list(
+          "default" = theme(
+            panel.grid.major.x = element_blank(),
+            axis.ticks.y = element_line(size = 0.25, color = "#2e2e2e")
+          ),
+          "intersections_matrix" = theme(
+            panel.grid = element_blank(),
+            panel.grid.major.y = element_line(color = c("#CFCCCF", "white"), size = 5)
+          ),
+          "Intersection size" = theme(
+            panel.grid = element_blank(),
+          ),
+          "overall_sizes" = theme(
+            panel.grid = element_blank(),
+            axis.ticks.x = element_line(size = 0.25, color = "#2e2e2e")
+          )
+        )
+      ),
+      stripes = alpha("white", 0)
+)
+
+pdf(paste0("combined/plots/Upset_TSS_",samplename,".pdf"),10,8)
+print(plot)
+dev.off()
+
