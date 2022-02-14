0d0442643b404a0ae5056f1bb3325585bce6ad22
joncahn <73137478+joncahn@users.noreply.github.com>
2022-02-14 15:11:31 -0500

    Update MaizeCode_shRNA_analysis.sh

--- scripts/MaizeCode_shRNA_analysis.sh
+++ scripts/MaizeCode_shRNA_analysis.sh
@@ -120,6 +120,6 @@ done < ${samplefile}
 printf "\nWaiting for samples to be processed individually\n"
 wait ${pids[*]}
 
-rm -f ${ref_dir}/temp_${datatype}_${ref}*
+rm -f ${ref_dir}/temp_${rnatype}_${ref}*
 
 printf "\nScript finished successfully!\n"
