5e70b653759764cda73c686eef22b793b1079569
joncahn <73137478+joncahn@users.noreply.github.com>
2022-03-22 10:37:57 -0400

    Update MaizeCode_shRNA_sample.sh

--- scripts/MaizeCode_shRNA_sample.sh
+++ scripts/MaizeCode_shRNA_sample.sh
@@ -184,8 +184,8 @@ elif [[ ${paired} == "SE" ]]; then
 	printf "\nMaking plus track for ${name}\n"
 	bamCoverage --filterRNAstrand forward -bs 1 -p ${threads} --normalizeUsing CPM -b mapped/${name}/filtered_${name}.bam -o tracks/${name}_plus.bw
 	bamCoverage --filterRNAstrand reverse -bs 1 -p ${threads} --normalizeUsing CPM -b mapped/${name}/filtered_${name}.bam -o tracks/${name}_minus.bw
-#	#### Filtering only small RNA sizes (15 to 32nt)
-#	seqkit seq --max-len 32 --min-len 15 fastq/filtered_${name}.fastq.gz | gzip -c > fastq/sized_${name}.fastq.gz
+	#### Filtering only small RNA sizes (15 to 32nt)
+	samtools view -h mapped/${name}/filtered_${name}.bam | awk 'length($10) <= 32 || $1 ~ /^@/' | samtools view -bS - > mapped/${name}/sized_${name}.bam
 	#### Getting stats of size distribution
   	printf "\nGetting stats for ${name}\n"
   	zcat fastq/trimmed_${name}.fastq.gz | awk '{if(NR%4==2) print length($1)}' | sort -n | uniq -c | awk -v OFS="\t" -v n=${name} '{print n,"trimmed",$2,$1}' > reports/sizes_trimmed_${name}.txt
