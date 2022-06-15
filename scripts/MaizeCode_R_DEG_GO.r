f75724735490de295263aa7d6059bc7ef337ae0e
joncahn <73137478+joncahn@users.noreply.github.com>
2022-06-15 17:24:41 -0400

    Update MaizeCode_R_DEG_GO.r

--- scripts/MaizeCode_R_DEG_GO.r
+++ scripts/MaizeCode_R_DEG_GO.r
@@ -124,12 +124,12 @@ getGO<-function(ont, name, sampletable) {
           rename("go" = "GO.ID", "term" = "Term") %>%
           mutate(parentTerm = term, score = scores)
   
-  if ( nrow(tab) > 1 ) {
+  if (! is.null(tab) && nrow(tab) > 1 ) {
   	simMatrix<-calculateSimMatrix(tab$GO.ID,
                                 orgdb="org.Zmays.eg.db",
                                 ont=ont,
                                 method="Rel")
-	if ( nrow(simMatrix) > 0 ) {
+	if (! is.null(simMatrix) && nrow(simMatrix) > 0 ) {
   	reducedTerms<-reduceSimMatrix(simMatrix,
   	                              scores,
   	                              threshold = 0.7,
