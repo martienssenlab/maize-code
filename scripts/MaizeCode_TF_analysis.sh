ae9b77e4ebb655ff3e1c4bf46ff7e9caab732608
joncahn <73137478+joncahn@users.noreply.github.com>
2022-12-09 13:38:23 -0500

    Update MaizeCode_TF_analysis.sh

--- scripts/MaizeCode_TF_analysis.sh
+++ scripts/MaizeCode_TF_analysis.sh
@@ -11,9 +11,10 @@
 usage="
 ##### Script for Maize code TF ChIP data analysis, used by script MaizeCode_analysis.sh for TF data
 #####
-##### sh MaiCode_TF_analysis.sh -f samplefile [-h]
+##### sh MaiCode_TF_analysis.sh -f samplefile [-a mappingoption] [-h]
 #####	-f: samplefile containing the samples to compare and the reference directory in 6 tab-delimited columns:
 ##### 		Line, TF, ChIP, PE or SE, Reference directory
+#####	-a: mapping option [ default | colcen ]
 ##### 	-h: help, returns usage
 ##### 
 ##### It merges the two replicate files, and creates pseudo-replicates by splitting the merged bam file into 2 halves
@@ -38,11 +39,12 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-while getopts ":f:h" opt; do
+while getopts ":f:a:h" opt; do
 	case ${opt} in
 		h) 	printf "${usage}\n"
 			exit 0;;
 		f) 	export samplefile=${OPTARG};;
+		a)	export mapparam=${OPTARG};;
 		*)	printf "${usage}\n"
 			exit 1;;
 	esac
@@ -51,6 +53,17 @@ shift $((OPTIND - 1))
 
 if [ ! ${samplefile} ]; then
 	printf "Samplefile missing!\n"
+	printf "${usage}\n"
+	exit 1
+fi
+
+if [ ! ${mapparam} ]; then
+	printf "No mapping option selected, using default\n"
+	export mapparam="default"
+elif [[ "${mapparam}" == "default" ]] || [[ "${mapparam}" == "colcen" ]]; then
+	printf "${mapparam} chosen as the mapping option\n"
+else
+	printf "Unknown mapping option selected\n"
 	printf "${usage}\n"
 	exit 1
 fi
