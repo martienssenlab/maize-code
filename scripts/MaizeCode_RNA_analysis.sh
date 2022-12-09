8c53d05bc31c0754ab849b511a37bd6eab25a756
joncahn <73137478+joncahn@users.noreply.github.com>
2022-12-09 13:38:33 -0500

    Update MaizeCode_RNA_analysis.sh

--- scripts/MaizeCode_RNA_analysis.sh
+++ scripts/MaizeCode_RNA_analysis.sh
@@ -11,9 +11,10 @@
 usage="
 ##### Script for Maize code RNA data analysis, used by script MaizeCode_analysis.sh for RNA data
 #####
-##### sh MaiCode_RNA_analysis.sh -f samplefile [-h]
+##### sh MaiCode_RNA_analysis.sh -f samplefile [-a mappingoption] [-h]
 #####	-f: samplefile containing the samples to compare and in 5 tab-delimited columns:
 ##### 		Line, Tissue, Mark, PE or SE, Reference genome directory
+#####	-a: mapping option [ default | colcen ]
 ##### 	-h: help, returns usage
 ##### 
 ##### It merges the two replicate files and creates stranded bigwig files for each sample
@@ -36,11 +37,12 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-while getopts ":f:sh" opt; do
+while getopts ":f:a:h" opt; do
 	case ${opt} in
 		h) 	printf "${usage}\n"
 			exit 0;;
 		f) 	export samplefile=${OPTARG};;
+		a)	export mapparam=${OPTARG};;
 		*)	printf "${usage}\n"
 			exit 1;;
 	esac
@@ -49,6 +51,17 @@ shift $((OPTIND - 1))
 
 if [ ! ${samplefile} ]; then
 	printf "Samplefile missing!\n"
+	printf "${usage}\n"
+	exit 1
+fi
+
+if [ ! ${mapparam} ]; then
+	printf "No mapping option selected, using default\n"
+	export mapparam="default"
+elif [[ "${mapparam}" == "default" ]] || [[ "${mapparam}" == "colcen" ]]; then
+	printf "${mapparam} chosen as the mapping option\n"
+else
+	printf "Unknown mapping option selected\n"
 	printf "${usage}\n"
 	exit 1
 fi
