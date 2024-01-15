89998bab859176fd71dc610a0587eb7ad3291520
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2024-01-15 08:55:21 -0500

    Update MaizeCode_line_analysis.sh

--- scripts/MaizeCode_line_analysis.sh
+++ scripts/MaizeCode_line_analysis.sh
@@ -465,6 +465,7 @@ if [ ${#chip_sample_list[@]} -ge 1 ]; then
 		case "${sample}" in
 			*H3K4me1*|*H3K27me1*|*H3K27me2*|*H3K27me3*|*H3K9me1*|*H3K9me2*|*H3K9me3*|*H4K20me1*|*H4K20me2*|*H4K20me3*|*DDM1*|*H3.3*|*H3.1*|*MGH3*|*CenH3*|*PolIIS2P*|*H2AT120Ph*) export peaktype="broad";;
 			*H3K27ac*|*H3K4me3*|*H3K9ac*|*H3K23ac*|*H4K16ac*|*MNase*) export peaktype="narrow";;
+  			*H3*)	 export peaktype="broad";;
 		esac
 		awk -v OFS="\t" -v s=${sample} '($1~/^[0-9]/ || $1~/^chr[0-9]/ || $1~/^Chr[0-9]/ ) {print $1,$2,$3,s}' ChIP/peaks/best_peaks_${sample}.bed >> combined/peaks/tmp_peaks_${analysisname}.bed
 	done
