1ddc9d8ff8bbc1efbd7894654744406f91d0baff
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2024-01-15 08:56:40 -0500

    Update MaizeCode_ChIP_analysis.sh

--- scripts/MaizeCode_ChIP_analysis.sh
+++ scripts/MaizeCode_ChIP_analysis.sh
@@ -142,6 +142,7 @@ do
 		case "${mark}" in
 			H3K4me1|H3K27me1|H3K27me2|H3K27me3|H3K9me1|H3K9me2|H3K9me3|H4K20me1|H4K20me2|H4K20me3|*DDM1|H3.3|H3.1|MGH3|CenH3|PolIIS2P|H2AT120Ph) export peaktype="broad";;
 			H3K27ac|H3K4me3|H3K9ac|H4K16ac|H3K23ac|MNase) export peaktype="narrow";;
+   			H3)	export peaktype="broad";;
 		esac
 		if [[ "${samplerep}" == "two" ]]; then
 			listfiletypes=("merged" "Rep1" "Rep2" "pseudo1" "pseudo2")
