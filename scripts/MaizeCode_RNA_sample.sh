57f614540fa5ae83d55287e9f68fcbd7c393e312
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2024-07-25 13:32:18 -0400

    Update MaizeCode_RNA_sample.sh

--- scripts/MaizeCode_RNA_sample.sh
+++ scripts/MaizeCode_RNA_sample.sh
@@ -135,8 +135,7 @@ if [[ ${paired} == "PE" ]]; then
 	STAR --version
 	if [[ ${mapparam} == "heavy" ]]; then
 		STAR --runMode alignReads --genomeDir ${ref_dir}/STAR_index --readFilesIn ${filesorder} --readFilesCommand zcat --runThreadN ${threads} --genomeLoad NoSharedMemory --outMultimapperOrder Random --outFileNamePrefix mapped/map_${name}_ --outSAMtype BAM SortedByCoordinate --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outFilterMultimapNmax 20 --quantMode GeneCounts --limitBAMsortRAM 6087171028 --outBAMsortingBinsN 200 --outBAMsortingThreadN 1
-		### Marking duplicates
- 		STAR --runMode inputAlignmentsFromBAM --inputBAMfile mapped/map_${name}_Aligned.sortedByCoord.out.bam --bamRemoveDuplicatesType UniqueIdentical --outFileNamePrefix mapped/mrkdup_${name}_ --limitBAMsortRAM 6087171028 --outBAMsortingBinsN 200 --outBAMsortingThreadN 1
+		### Marking duplicates too intensive
 	else
  		STAR --runMode alignReads --genomeDir ${ref_dir}/STAR_index --readFilesIn ${filesorder} --readFilesCommand zcat --runThreadN ${threads} --genomeLoad NoSharedMemory --outMultimapperOrder Random --outFileNamePrefix mapped/map_${name}_ --outSAMtype BAM SortedByCoordinate --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outFilterMultimapNmax 20 --quantMode GeneCounts
    		### Marking duplicates
